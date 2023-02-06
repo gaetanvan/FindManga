72477f982dc8013c5c65c4ea3a05bf3d54ab76e3
gaetanvan <61145812+gaetanvan@users.noreply.github.com>
2023-02-06 16:54:46 +0100

    1.1.0

--- app.js
+++ app.js
@@ -1,17 +1,129 @@
-function getManga(){
+const container = document.getElementById('container')
+
+if (window.location.search.length < 1) {
+const btnSearch = document.getElementById('searchBtn')
+btnSearch.addEventListener('click', getManga)
+}
+
+async function getManga(){
+
+    if (container.innerHTML.length > 0){
+        container.innerHTML = ''
+    }
+
+    let mangaUrl
+
+    if (document.getElementById('form1').value.length > 0){
     let title = document.getElementById('form1').value;
-    const baseUrl = 'https://api.mangadex.org';
+        mangaUrl = 'https://api.mangadex.org/manga?title=' + title + '&limit=100&order[followedCount]=desc&contentRating[]=safe';
+    }
+    else{
+        mangaUrl = 'https://api.mangadex.org/manga?limit=100&order[followedCount]=desc&contentRating[]=safe'
+    }
 
-    (async () => {
-        const resp = await axios({
-            method: 'GET',
-            url: `${baseUrl}/manga`,
-            params: {
-                title: title
+    let mangaFetch = await fetch(mangaUrl)
+    let mangaResult = await mangaFetch.json();
+
+    for(let i = 0; i <= 11 ; i++){
+
+        let coverId;
+    if (mangaResult.data[i].relationships) {
+        for (let e = 0; e <= mangaResult.data[i].relationships.length - 1; e++) {
+            if (mangaResult.data[i].relationships[e].type === 'cover_art') {
+                coverId = mangaResult.data[i].relationships[e].id
+                break;
             }
-        });
+        }
+    }
+        let coverUrl = 'https://api.mangadex.org/cover/' + coverId;
 
-        console.log(resp.data.data.map(manga => manga.id));
-    })();
+        let coverFetch = await fetch(coverUrl);
+        let resultCover = await coverFetch.json();
+        let srcCover = resultCover.data.attributes.fileName
 
+        console.log(mangaResult.data[i])
+
+        let lastChapterUrl = 'https://api.mangadex.org/manga/'+ mangaResult.data[i].id +'/feed?order[chapter]=desc&limit=1'
+        let lastChapterFetch = await fetch(lastChapterUrl);
+        let lastChapter = await lastChapterFetch.json();
+        let lastChapterNumber
+
+
+        if (lastChapter.data.length > 0) {
+            lastChapterNumber = lastChapter.data[0].attributes.chapter
+        }
+        else {
+            lastChapterNumber = 'Inconnu'
+        }
+        container.innerHTML +=
+            '<a href="manga.html?id='+ mangaResult.data[i].id +'" class="col-3 text-decoration-none fw-bold text-reset mt-3">'+
+                '<div class="card m-auto homeCard darkblue">' +
+                '<img src="https://uploads.mangadex.org/covers/'+ mangaResult.data[i].id +'/'+ srcCover +'" class="card-img-top homeImg" alt="...">' +
+                '<div class="card-body row align-items-center">' +
+                    '<div class="col">' +
+                        '<p class="bold card-text text-center bold">'+ mangaResult.data[i].attributes.title.en +'</p>' +
+                        '<p class="card-text text-center">Nombre de chapitre : '+ lastChapterNumber +'</p>' +
+                    '</div>' +
+                '</div>' +
+                '</div>' +
+            '</a>'
+    }
+}
+if (window.location.search.length < 1) {
+    getManga();
+}
+else {
+    const paramsString = window.location.search
+    const searchParams = new URLSearchParams(paramsString)
+    let mangaId = searchParams.get('id');
+    let mangaUrl = 'https://api.mangadex.org/manga/' + mangaId
+
+    let mangaFetch = await fetch(mangaUrl)
+    let mangaResult = await mangaFetch.json()
+
+    console.log(mangaResult)
+
+    let coverId;
+    if (mangaResult.data.relationships) {
+        for (let e = 0; e <= mangaResult.data.relationships.length - 1; e++) {
+            if (mangaResult.data.relationships[e].type === 'cover_art') {
+                coverId = mangaResult.data.relationships[e].id
+                break;
+            }
+        }
+    }
+    let coverUrl = 'https://api.mangadex.org/cover/' + coverId;
+
+    let coverFetch = await fetch(coverUrl);
+    let resultCover = await coverFetch.json();
+    let srcCover = resultCover.data.attributes.fileName
+
+    console.log(srcCover)
+
+    container.innerHTML += '<div class="row">' +
+        '           <div class="col-md-8">' +
+        '                <div class="row">' +
+        '                    <div class="col-md-12 text-center fw-bold fs-4">' +
+                                mangaResult.data.attributes.title.en +
+        '                    </div>' +
+        '                </div>' +
+        '                <div class="row mt-4">' +
+        '                    <div class="col-md-12">' +
+                                mangaResult.data.attributes.description.en +
+        '                    </div>' +
+        '                </div>' +
+        '            </div>' +
+        '            <div class="col-md-4">' +
+        '                <div class="row">' +
+        '                    <div class="col-md-12">' +
+        '                        <img src="https://uploads.mangadex.org/covers/'+ mangaResult.data.id +'/'+ srcCover +'" class="card-img-top homeImg" alt="...">' +
+        '                    </div>' +
+        '                </div>' +
+        '                <div class="row">' +
+        '                    <div class="col-md-12">' +
+        '                        author' +
+        '                    </div>' +
+        '                </div>' +
+        '            </div>' +
+        '        </div>';
 }
